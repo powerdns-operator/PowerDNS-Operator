edf578a62683db41325560f5162a43f18452dae6
Anthony Treuillier <anthony.treuillier@gmail.com>
2026-06-12 17:38:30 +0200

    refactor: Use helpers to manage Status (#321)
    * refactor: Use helpers to manage Status
    
    From now, Status are managed through helpers functions
    Update in the cluster are managed through a `defer` function
    Main principle, exit as soon as possible
    
    Signed-off-by: Anthony TREUILLIER <anthony.treuillier@gmail.com>
    
    * Update internal/controller/zone_controller.go
    
    Co-authored-by: Benjamin Pinchon <dev@mdfr.me>
    Signed-off-by: Anthony Treuillier <anthony.treuillier@gmail.com>
    
    ---------
    
    Signed-off-by: Anthony TREUILLIER <anthony.treuillier@gmail.com>
    Signed-off-by: Anthony Treuillier <anthony.treuillier@gmail.com>
    Co-authored-by: Benjamin Pinchon <dev@mdfr.me>

--- api/v1alpha2/constants.go
+++ api/v1alpha2/constants.go
@@ -5,3 +5,17 @@ const (
 	PENDING_STATUS   = "Pending"
 	SUCCEEDED_STATUS = "Succeeded"
 )
+
+const (
+	MISSING_ZONE_REASON            = "ZoneMissing"
+	MISSING_ZONE_MESSAGE           = "Missing Zone:"
+	ZONE_NOT_AVAILABLE_REASON      = "ZoneNotAvailable"
+	ZONE_NOT_AVAILABLE_MESSAGE     = "Zone not available:"
+	DUPLICATED_REASON              = "Duplicated"
+	RRSET_DUPLICATED_MESSAGE       = "At least another ClusterRRset/RRset exists with the same name"
+	SYNCHRONIZATION_FAILED_REASON  = "SynchronizationFailed"
+	SYNCHRONIZATION_FAILED_MESSAGE = "Synchronization failed:"
+	SUCCEEDED_REASON               = "Succeeded"
+	SUCCEEDED_MESSAGE              = "Succeeded"
+	ZONE_DUPLICATED_MESSAGE        = "At least another ClusterZone/Zone exists with the same name"
+)
