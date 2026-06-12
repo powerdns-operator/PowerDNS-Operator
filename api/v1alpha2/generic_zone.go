e4e1566da25fc669fbb6a0bda57c400a66fc504d
Anthony TREUILLIER <anthony.treuillier@gmail.com>
2026-06-12 15:00:05 +0200

    refactor: Use helpers to manage Status
    From now, Status are managed through helpers functions
    Update in the cluster are managed through a `defer` function
    Main principle, exit as soon as possible
    
    Signed-off-by: Anthony TREUILLIER <anthony.treuillier@gmail.com>

--- api/v1alpha2/generic_zone.go
+++ api/v1alpha2/generic_zone.go
@@ -13,8 +13,13 @@
 package v1alpha2
 
 import (
+	"time"
+
+	"github.com/joeig/go-powerdns/v3"
+	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	runtime "k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/utils/ptr"
 )
 
 // +kubebuilder:object:root=false
@@ -35,6 +40,11 @@ type GenericZone interface {
 	GetStatus() ZoneStatus
 	SetStatus(status ZoneStatus)
 	Copy() GenericZone
+
+	// Set Status functions
+	SetDuplicated()
+	SetSynchronizationFailed(err error)
+	SetAvailable(zoneRes *powerdns.Zone)
 }
 
 // +kubebuilder:object:root:false
@@ -65,6 +75,18 @@ func (c *Zone) Copy() GenericZone {
 	return c.DeepCopy()
 }
 
+func (c *Zone) SetDuplicated() {
+	setZoneDuplicated(&c.Status, c.Generation)
+}
+
+func (c *Zone) SetSynchronizationFailed(err error) {
+	setZoneSynchronizationFailed(&c.Status, c.Generation, err)
+}
+
+func (c *Zone) SetAvailable(zoneRes *powerdns.Zone) {
+	setZoneAvailable(&c.Status, c.Generation, zoneRes)
+}
+
 // +kubebuilder:object:root:false
 // +kubebuilder:object:generate:false
 var _ GenericZone = &ClusterZone{}
@@ -91,4 +113,64 @@ func (c *ClusterZone) SetStatus(status ZoneStatus) {
 
 func (c *ClusterZone) Copy() GenericZone {
 	return c.DeepCopy()
+}
+
+func (c *ClusterZone) SetDuplicated() {
+	setZoneDuplicated(&c.Status, c.Generation)
+}
+
+func (c *ClusterZone) SetSynchronizationFailed(err error) {
+	setZoneSynchronizationFailed(&c.Status, c.Generation, err)
+}
+
+func (c *ClusterZone) SetAvailable(zoneRes *powerdns.Zone) {
+	setZoneAvailable(&c.Status, c.Generation, zoneRes)
+}
+
+func setZoneDuplicated(status *ZoneStatus, generation int64) {
+	status.SyncStatus = ptr.To(FAILED_STATUS)
+	status.ObservedGeneration = &generation
+	condition := metav1.Condition{
+		Type:               "Available",
+		Status:             metav1.ConditionFalse,
+		LastTransitionTime: metav1.Time{Time: time.Now().UTC()},
+		Reason:             DUPLICATED_REASON,
+		Message:            ZONE_DUPLICATED_MESSAGE,
+	}
+	meta.SetStatusCondition(&status.Conditions, condition)
+}
+
+func setZoneSynchronizationFailed(status *ZoneStatus, generation int64, err error) {
+	status.SyncStatus = ptr.To(FAILED_STATUS)
+	status.ObservedGeneration = &generation
+	condition := metav1.Condition{
+		Type:               "Available",
+		Status:             metav1.ConditionFalse,
+		LastTransitionTime: metav1.Time{Time: time.Now().UTC()},
+		Reason:             SYNCHRONIZATION_FAILED_REASON,
+		Message:            SYNCHRONIZATION_FAILED_MESSAGE + err.Error(),
+	}
+	meta.SetStatusCondition(&status.Conditions, condition)
+}
+
+func setZoneAvailable(status *ZoneStatus, generation int64, zoneRes *powerdns.Zone) {
+	status.SyncStatus = ptr.To(SUCCEEDED_STATUS)
+	status.ObservedGeneration = &generation
+	status.ID = zoneRes.ID
+	status.Name = zoneRes.Name
+	status.Kind = ptr.To(string(ptr.Deref(zoneRes.Kind, "")))
+	status.Serial = zoneRes.Serial
+	status.NotifiedSerial = zoneRes.NotifiedSerial
+	status.EditedSerial = zoneRes.EditedSerial
+	status.Masters = zoneRes.Masters
+	status.DNSsec = zoneRes.DNSsec
+	status.Catalog = zoneRes.Catalog
+	condition := metav1.Condition{
+		Type:               "Available",
+		Status:             metav1.ConditionTrue,
+		LastTransitionTime: metav1.Time{Time: time.Now().UTC()},
+		Reason:             SUCCEEDED_REASON,
+		Message:            SUCCEEDED_MESSAGE,
+	}
+	meta.SetStatusCondition(&status.Conditions, condition)
 }
--- internal/controller/clusterrrset_controller.go
+++ internal/controller/clusterrrset_controller.go
@@ -19,7 +19,6 @@ import (
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/utils/ptr"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
@@ -51,10 +50,12 @@ func (r *ClusterRRsetReconciler) Reconcile(ctx context.Context, req ctrl.Request
 
 	// RRset
 	rrset := &dnsv1alpha2.ClusterRRset{}
+	log.V(1).Info("Getting RRset", "ClusterRRset.Name", req.Name)
 	err := r.Get(ctx, req.NamespacedName, rrset)
 	if err != nil {
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
+	log.V(1).Info("RRset found", "RRset", rrset)
 
 	// Initialize variable to represent ClusterRRset situation
 	isModified := rrset.Status.ObservedGeneration != nil && *rrset.Status.ObservedGeneration != rrset.GetGeneration()
@@ -63,13 +64,16 @@ func (r *ClusterRRsetReconciler) Reconcile(ctx context.Context, req ctrl.Request
 	if rrset.Status.LastUpdateTime != nil {
 		lastUpdateTime = rrset.Status.LastUpdateTime
 	}
+	log.V(1).Info("ClusterRRset situation", "isModified", isModified, "isDeleted", isDeleted, "lastUpdateTime", lastUpdateTime)
 
 	// Position metrics finalizer as soon as possible
 	if !isDeleted {
+		log.V(1).Info("ClusterRRset not deleted", "RRset.Name", rrset.Name)
 		// The object is not being deleted, so if it does not have our finalizer,
 		// then lets add the finalizer and update the object. This is equivalent
 		// to registering our finalizer.
 		if !controllerutil.ContainsFinalizer(rrset, METRICS_FINALIZER_NAME) {
+			log.V(1).Info("Adding finalizer to ClusterRRset")
 			controllerutil.AddFinalizer(rrset, METRICS_FINALIZER_NAME)
 			lastUpdateTime = &metav1.Time{Time: time.Now().UTC()}
 			if err := r.Update(ctx, rrset); err != nil {
@@ -79,15 +83,19 @@ func (r *ClusterRRsetReconciler) Reconcile(ctx context.Context, req ctrl.Request
 		}
 	}
 
-	// When updating a ClusterRRset, if 'Status' is not changed, 'LastTransitionTime' will not be updated
-	// So we delete condition to force new 'LastTransitionTime'
 	original := rrset.DeepCopy()
-	if !isDeleted && isModified {
-		meta.RemoveStatusCondition(&rrset.Status.Conditions, "Available")
+	// Ensure we update the status in case of early return
+	defer func() {
 		if err := r.Status().Patch(ctx, rrset, client.MergeFrom(original)); err != nil {
 			log.Error(err, "unable to patch ClusterRRSet status")
-			return ctrl.Result{}, err
 		}
+	}()
+
+	// When updating a ClusterRRset, if 'Status' is not changed, 'LastTransitionTime' will not be updated
+	// So we delete condition to force new 'LastTransitionTime'
+	if !isDeleted && isModified {
+		log.V(1).Info("Removing Available condition from ClusterRRset")
+		meta.RemoveStatusCondition(&rrset.Status.Conditions, "Available")
 	}
 
 	// Zone
@@ -100,16 +108,20 @@ func (r *ClusterRRsetReconciler) Reconcile(ctx context.Context, req ctrl.Request
 	case "ClusterZone":
 		zone = &dnsv1alpha2.ClusterZone{}
 	}
+	log.V(1).Info("Getting associated Zone", "Zone.Name", rrset.Spec.ZoneRef.Name, "Zone.Kind", rrset.Spec.ZoneRef.Kind)
 	err = r.Get(ctx, client.ObjectKey{Namespace: rrset.Namespace, Name: rrset.Spec.ZoneRef.Name}, zone)
 	if err != nil {
 		if apierrors.IsNotFound(err) {
 			// Zone not found, remove finalizer and requeue
+			log.V(1).Info("Zone not found", "Zone.Name", rrset.Spec.ZoneRef.Name)
 			actionOnFinalizer := false
 			if controllerutil.ContainsFinalizer(rrset, RESOURCES_FINALIZER_NAME) {
+				log.V(1).Info("Removing resources finalizer from ClusterRRset")
 				controllerutil.RemoveFinalizer(rrset, RESOURCES_FINALIZER_NAME)
 				actionOnFinalizer = true
 			}
 			if isDeleted && controllerutil.ContainsFinalizer(rrset, METRICS_FINALIZER_NAME) {
+				log.V(1).Info("Removing metrics finalizer from ClusterRRset")
 				controllerutil.RemoveFinalizer(rrset, METRICS_FINALIZER_NAME)
 				// Remove resource metrics
 				removeRrsetMetrics(rrset)
@@ -124,54 +136,32 @@ func (r *ClusterRRsetReconciler) Reconcile(ctx context.Context, req ctrl.Request
 
 			// If RRset is under deletion, no need to update its status
 			if !isDeleted {
-				original = rrset.DeepCopy()
-				rrset.Status.SyncStatus = ptr.To(dnsv1alpha2.PENDING_STATUS)
-				rrset.Status.ObservedGeneration = &rrset.Generation
-				meta.SetStatusCondition(&rrset.Status.Conditions, metav1.Condition{
-					Type:               "Available",
-					Status:             metav1.ConditionFalse,
-					LastTransitionTime: metav1.NewTime(time.Now().UTC()),
-					Reason:             RrsetReasonZoneNotAvailable,
-					Message:            RrsetMessageNonExistentZone + err.Error(),
-				})
-				if err := r.Status().Patch(ctx, rrset, client.MergeFrom(original)); err != nil {
-					log.Error(err, "unable to patch RRSet status")
-					return ctrl.Result{}, err
-				}
+				log.V(1).Info("ClusterRRset is not deleted, setting missing zone")
+				rrset.SetMissingZone(err)
 				updateRrsetsMetrics(getRRsetName(rrset), rrset)
 			}
 
 			// Race condition when creating Zone+RRset at the same time
 			// RRset is not created because Zone is not created yet
 			// Requeue after few seconds
+			log.V(1).Info("Requeuing ClusterRRset", "RequeueAfter", 2*time.Second)
 			return ctrl.Result{RequeueAfter: 2 * time.Second}, nil
 		} else {
 			log.Error(err, "Failed to get zone")
+			rrset.SetZoneNotAvailable(zone.GetName())
 			return ctrl.Result{}, err
 		}
 	}
 	// If a Zone/ClusterZone exists but is in Failed Status
 	zoneIsInFailedStatus := (zone.GetStatus().SyncStatus != nil && *zone.GetStatus().SyncStatus == dnsv1alpha2.FAILED_STATUS)
 	if zoneIsInFailedStatus {
-		original = rrset.DeepCopy()
-		rrset.Status.SyncStatus = ptr.To(dnsv1alpha2.FAILED_STATUS)
-		rrset.Status.ObservedGeneration = &rrset.Generation
-		meta.SetStatusCondition(&rrset.Status.Conditions, metav1.Condition{
-			Type:               "Available",
-			Status:             metav1.ConditionFalse,
-			LastTransitionTime: metav1.NewTime(time.Now().UTC()),
-			Reason:             RrsetReasonZoneNotAvailable,
-			Message:            RrsetMessageUnavailableZone + zone.GetName(),
-		})
-		if err := r.Status().Patch(ctx, rrset, client.MergeFrom(original)); err != nil {
-			log.Error(err, "unable to patch RRSet status")
-			return ctrl.Result{}, err
-		}
-
+		log.V(1).Info("Zone is in failed status, setting zone not available")
+		rrset.SetZoneNotAvailable(zone.GetName())
 		// Update metrics
 		updateRrsetsMetrics(getRRsetName(rrset), rrset)
 
 		if isDeleted {
+			log.V(1).Info("ClusterRRset is deleted, removing metrics finalizer")
 			if controllerutil.ContainsFinalizer(rrset, METRICS_FINALIZER_NAME) {
 				controllerutil.RemoveFinalizer(rrset, METRICS_FINALIZER_NAME)
 				// Remove resource metrics
