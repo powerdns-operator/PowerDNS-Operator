0edad4738954dba0fa4cc7ad3c5df8b0922e1cb3
Anthony Treuillier <anthony.treuillier@gmail.com>
2026-06-12 17:56:17 +0200

    chore: go1.26.4 and dependencies (#323)
    Signed-off-by: Anthony TREUILLIER <anthony.treuillier@gmail.com>

--- api/v1alpha2/groupversion_info.go
+++ api/v1alpha2/groupversion_info.go
@@ -24,7 +24,7 @@ var (
 	GroupVersion = schema.GroupVersion{Group: "dns.cav.enablers.ob", Version: "v1alpha2"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
-	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
+	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion} //nolint:staticcheck // Waiting for kubebuilder proposal to fix this
 
 	// AddToScheme adds the types in this group-version to the given scheme.
 	AddToScheme = SchemeBuilder.AddToScheme
